37b2f4ea3f8c09c1219224545128fb4f2c6110ac
Алексей Кожакин <alexeykozhakin@yandex.ru>
2024-05-19 20:46:46 +0200

    Task 16 done -tests for Loto

--- 16/test_loto.py
+++ 16/test_loto.py
@@ -10,33 +10,17 @@ class TestProgram(unittest.TestCase):
     def test_1(self):
         self.player = loto.Player('Alex')
         self.assertEqual(self.player.name, 'Alex')
-        print("Test 1 passed")
-    # def setUp(self):
-        # # Подготовка к тестам, создание экземпляра игры и другие начальные действия
-        # self.new_game = loto.Game()
-        # self.test_player = loto.Player('Alex')
-        # self.new_game.list_players.append(self.test_player)
-    # def test_initial_state(self):
-        # # Тестирование начального состояния игры
-        # #self.assertEqual(len(self.game.cards), 3)  # Проверяем количество карточек
-        # self.assertEqual(self.new_game.list_players[0].name, 'Alex')  # Проверяем количество карточек
-        # #self.assertTrue(all(card.is_empty() for card in self.game.cards))  # Проверяем, что все карточки пусты
-        #self.assertTrue(all(card.is_empty() for card in self.game.cards))  # Проверяем, что все карточки пусты
-    # def test_gameplay(self):
-        # # Тестирование игрового процесса
-        # self.game.start_game()  # Начинаем игру
-        # self.assertFalse(self.game.is_over())  # Проверяем, что игра не закончена после начала
-        
-        # self.game.draw_number()  # Вытаскиваем номер
-        # self.assertEqual(len(self.game.drawn_numbers), 1)  # Проверяем, что был вытянут один номер
-        
-        # self.game.check_cards()  # Проверяем карточки после вытягивания номера
-        
-        # # Проверяем, что игра завершена после заполнения карточек
-        # while not self.game.is_over():
-            # self.game.draw_number()
-            # self.game.check_cards()
-        # self.assertTrue(self.game.is_over())
+        print("Test 1.0 passed")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_2(self):
+        self.barrel=loto.Barrel()
+        self.assertEqual(self.barrel.left_barrels, 90)
+        self.barrel.next_barrel()
+        self.assertEqual(self.barrel.left_barrels, 89)
+        print("Test 2.0 passed")
+
+    def test_3(self):
+        self.card=loto.Card('Alex')
+        self.assertEqual(self.card.name,'Alex')
+        self.assertEqual(self.card.card_numbers,[])  
+        print('Test 3.0 passed')
